5860fcce4a04f1ef10e2fa857e7bcaa07e6c0b47
alan1898 <alan1898m@outlook.com>
2020-12-26 21:19:29 +0800

    parameterization

--- src/SIR.py
+++ src/SIR.py
@@ -56,48 +56,56 @@ def get_init_data(N, I0, R0):
 if __name__ == '__main__':
     np.set_printoptions(suppress=True)
 
+    # set params
+    country = 'Italy'  # country
+    N = 60000000  # total population
+    train_start = '3/31/20'
+    train_end = '4/3/20'
+    valid_start = '4/4/20'
+    pre_num = 730
+
     # read input data
     confirmed_global = pd.read_csv('../data/time_series_covid19_confirmed_global.csv')
     recovered_global = pd.read_csv('../data/time_series_covid19_recovered_global.csv')
     deaths_global = pd.read_csv('../data/time_series_covid19_deaths_global.csv')
 
-    confirmed = confirmed_global[confirmed_global['Country/Region'] == 'Italy'].drop(['Province/State', 'Country/Region', 'Lat', 'Long'], axis=1).sum()
-    recovered = recovered_global[recovered_global['Country/Region'] == 'Italy'].drop(['Province/State', 'Country/Region', 'Lat', 'Long'], axis=1).sum()
-    deaths = deaths_global[deaths_global['Country/Region'] == 'Italy'].drop(['Province/State', 'Country/Region', 'Lat', 'Long'], axis=1).sum()
+    confirmed = confirmed_global[confirmed_global['Country/Region'] == country].drop(['Province/State', 'Country/Region', 'Lat', 'Long'], axis=1).sum()
+    recovered = recovered_global[recovered_global['Country/Region'] == country].drop(['Province/State', 'Country/Region', 'Lat', 'Long'], axis=1).sum()
+    deaths = deaths_global[deaths_global['Country/Region'] == country].drop(['Province/State', 'Country/Region', 'Lat', 'Long'], axis=1).sum()
 
     infectious = confirmed - recovered - deaths
     recovered = recovered + deaths
 
     # training set
-    infectious_train = infectious.loc['3/31/20':'4/3/20']
-    recovered_train = recovered.loc['3/31/20':'4/3/20']
+    infectious_train = infectious.loc[train_start:train_end]
+    recovered_train = recovered.loc[train_start:train_end]
     # print(infectious_train)
     # print(recovered_train)
 
     # validation set
-    infectious_valid = infectious.loc['4/4/20':]
-    recovered_valid = recovered.loc['4/4/20':]
+    infectious_valid = infectious.loc[valid_start:]
+    recovered_valid = recovered.loc[valid_start:]
 
     # get initial data
-    N = 60000000  # total population
-    I0 = 77635
-    R0 = 28157
+    I0 = infectious_train.loc[train_start]
+    R0 = recovered_train.loc[train_start]
     Y0 = get_init_data(N, I0, R0)
 
     # train
     model = SIRModel(0.0001, 0.0001, 'L-BFGS-B')
     model.fit(Y0, infectious_train, recovered_train)
     best_params = model.get_optimal_params()
+    best_params = model.get_optimal_params()
+    print(best_params)
 
     # predict
-    i0 = 85388
-    r0 = 34439
+    i0 = infectious_train.loc[train_end]
+    r0 = recovered_train.loc[train_end]
     y0 = get_init_data(N, i0, r0)
-    predict_result = model.predict(y0, 730)
-    # print(predict_result)
+    predict_result = model.predict(y0, pre_num)
 
     t = np.linspace(1, len(infectious), len(infectious))
-    t_predict = np.linspace(64, 793, 730)
+    t_predict = np.linspace(64, 793, pre_num)
 
     fig = plt.figure(facecolor='w', dpi=100)
     ax = fig.add_subplot(111)
@@ -108,10 +116,10 @@ if __name__ == '__main__':
     # predicted I and R
     ax.plot(t_predict, predict_result[:, 1], 'r-.', alpha=0.5, lw=2, label='infectious_predict')
     ax.plot(t_predict, predict_result[:, 2], 'g-.', alpha=0.5, lw=2, label='recovered_predict')
-    # 设置横纵坐标轴
+
     ax.set_xlabel('Time/days')
     ax.set_ylabel('Number')
-    # 添加图例
+
     legend = ax.legend()
     ax.grid(axis='y')
     plt.box(False)
